f00f470b99cdb059a71057c22029435ebecec373
Past <pastplayz@gmail.com>
2022-09-03 13:13:04 +0530

    Added Util commands, set up mongodb local database and making logs

--- src/commands.ts
+++ src/commands.ts
@@ -1,5 +1,6 @@
 import { Command } from "./Command";
 import { Help } from "./Commands/Help";
 import { Hug } from "./Commands/Hug";
+import { SetLogChannel } from "./Commands/Utils/SetLogChannel";
 
-export var commands: Command[] = [Help, Hug];
+export var commands: Command[] = [Help, Hug, SetLogChannel];
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import Client from 'discord.js';
 import { TOKEN } from './secrets';
 import ready from './Listeners/ready';
 import interactionCreate from './Listeners/interactionCreate';
+import Logs from './Listeners/Logs';
+import connect from './Database/connect';
 
 
 const client = new Client.Client({
     intents: 'DirectMessages'
 });
 
+connect()
 ready(client);
 interactionCreate(client);
+Logs(client);
 
 client.login(TOKEN)
