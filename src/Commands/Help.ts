670f57f89bb2acc13bff179b5c1e8003587de1dc
Past <pastplayz@gmail.com>
2022-09-05 11:09:20 +0530

    Made Major changes - Added more commands, events, classes, etc.

--- src/Commands/Help.ts
+++ src/Commands/Help.ts
@@ -1,10 +1,31 @@
+import { ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SelectMenuBuilder } from "discord.js";
 import { Command } from "../Structures/Command";
 
 export default new Command ({
     name: "help",
     description: "Stop it. Get some help.",
     run: async({client, interaction, args}) => {
-        interaction.reply("hey!")
+        const embedo = new EmbedBuilder({
+            title: "Pasta helps you!",
+            description: "many many headaches:(",
+            fields: [
+                { "name": "⚙️ Configuration", "value": "Configure the bot to your server, for your liking!", "inline": true },
+                { "name": "🎉 Fun", "value": "Some super fun commands!", "inline": true},
+            ],
+            color: 0x0000FF,
+            footer: { text: "Pasta Help" },
+        })
+        const actionRow = new ActionRowBuilder<SelectMenuBuilder>()
+        .addComponents(
+            new SelectMenuBuilder()
+            .setCustomId("help")
+            .setPlaceholder("Choose something to get help with... ")
+            .addOptions(
+                { label: "Configuration", emoji: "⚙️", value: "config" },
+                { label: "Fun", emoji: "🎉", value: "fun" }
+            )
+        )
+        interaction.reply({ embeds: [embedo], components: [actionRow] })
         
     }
 })
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 require('dotenv').config();
 import { XClient } from "./Structures/Client";
+import { promisify } from "util";
+import { glob } from "glob";
+
+const globPromise = promisify(glob);
 
 
 export const client = new XClient();
